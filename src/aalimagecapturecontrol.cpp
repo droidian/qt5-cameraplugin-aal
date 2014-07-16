8677f8246bd869f623297683da0424a2a34db3bf
Florian Boucault <florian.boucault@canonical.com>
2014-07-16 11:10:50 -0300

    Send GPS location to the Android layer (libhybris).

--- src/aalimagecapturecontrol.cpp
+++ src/aalimagecapturecontrol.cpp
@@ -84,8 +84,20 @@ int AalImageCaptureControl::capture(const QString &fileName)
         return m_lastRequestId;
     }
 
-    int rotation = m_service->metadataWriterControl()->correctedOrientation();
+    AalMetaDataWriterControl* metadataControl = m_service->metadataWriterControl();
+
+    int rotation = metadataControl->correctedOrientation();
     android_camera_set_rotation(m_service->androidControl(), rotation);
+
+    double latitude = metadataControl->metaData("GPSLatitude").toDouble();
+    double longitude = metadataControl->metaData("GPSLongitude").toDouble();
+    double altitude = metadataControl->metaData("GPSAltitude").toDouble();
+    QDateTime timestamp = metadataControl->metaData("GPSTimeStamp").toDateTime();
+    QString processingMethod = metadataControl->metaData("GPSProcessingMethod").toString();
+    android_camera_set_location(m_service->androidControl(),
+                                (float)latitude, (float)longitude, (float)altitude,
+                                timestamp.toTime_t(),
+                                processingMethod.toLocal8Bit().constData());
 
     android_camera_take_snapshot(m_service->androidControl());
 
