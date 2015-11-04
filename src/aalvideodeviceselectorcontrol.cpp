53e8d3a3ee65943596c8b046a466f4ae06cdba4d
Florian Boucault <florian.boucault@canonical.com>
2015-11-04 15:33:44 +0100

    Fixed broken aspect ratio rendering when switching cameras on arale.

--- src/aalvideodeviceselectorcontrol.cpp
+++ src/aalvideodeviceselectorcontrol.cpp
@@ -82,9 +82,6 @@ void AalVideoDeviceSelectorControl::setSelectedDevice(int index)
         return;
 
     m_service->disconnectCamera();
-    m_service->viewfinderControl()->resetAllSettings();
-    m_service->imageEncoderControl()->resetAllSettings();
-    m_service->videoEncoderControl()->resetAllSettings();
     m_currentDevice = index;
     if (m_service->isCameraActive()) {
         m_service->connectCamera();
