ubports/qtubuntu-camera

Add a camera orientation override for some devices
Krillin (BQ Aquaris E4.5) & vegetahd (BQ Aquaris E5) give the
incorrect orientation to us. So, I decided to override it here.

src/aalcameraserviceplugin.cpp
@@ -17,13 +17,25 @@
 #include "aalcameraserviceplugin.h"
 #include "aalcameraservice.h"
 
+#include <QString>
 #include <QDebug>
 #include <QMetaType>
 #include <qgl.h>
 
+#include <hybris/properties/properties.h>
 #include <hybris/camera/camera_compatibility_layer.h>
 #include <hybris/camera/camera_compatibility_layer_capabilities.h>
 
+static QString getAndroidDeviceCodename() {
+    char deviceCodename[PROP_VALUE_MAX];
+
+    int result = property_get("ro.product.device", deviceCodename, "");
+    if (result != 0) {
+        return QString();
+    }
+
+    return QString(deviceCodename);
+}
 
 AalServicePlugin::AalServicePlugin()
 {
@@ -98,6 +110,22 @@ int AalServicePlugin::cameraOrientation(const QByteArray & device) const
     int result = android_camera_get_device_info(deviceID, &facing, &orientation);
     if (result != 0) {
         return 0;
+    }
+
+    if (facing == FRONT_FACING_CAMERA_TYPE) {
+        QString deviceCodename = getAndroidDeviceCodename();
+
+        if (deviceCodename == "krillin" || deviceCodename == "vegetahd") {
+            // krillin / vegetahd lies to us - the top of the front facing camera
+            // points to the right of the screen (viewed from the front), which means
+            // the camera image needs rotating by 270deg with the device in its natural
+            // orientation (portrait). It tells us the camera orientation is 90deg
+            // though (see https://launchpad.net/bugs/1567542)
+            // https://git.launchpad.net/oxide/tree/shared/browser/media/oxide_video_capture_device_hybris.cc#n92
+
+            // FIXME: is this the right place to do this?
+            orientation = 270;
+        }
     }
 
     // Android's orientation means differently compared to QT's orientation.

droidian/qt5-cameraplugin-aal

src/aalcameraserviceplugin.cpp
@@ -17,13 +17,25 @@
 #include "aalcameraserviceplugin.h"
 #include "aalcameraservice.h"
 
+#include <QString>
 #include <QDebug>
 #include <QMetaType>
 #include <qgl.h>
 
+#include <hybris/properties/properties.h>
 #include <hybris/camera/camera_compatibility_layer.h>
 #include <hybris/camera/camera_compatibility_layer_capabilities.h>
 
+static QString getAndroidDeviceCodename() {
+    char deviceCodename[PROP_VALUE_MAX];
+
+    int length = property_get("ro.product.device", deviceCodename, "");
+    if (length <= 0) {
+        return QString();
+    }
+
+    return QString(deviceCodename);
+}
 
 AalServicePlugin::AalServicePlugin()
 {
@@ -98,6 +110,22 @@ int AalServicePlugin::cameraOrientation(const QByteArray & device) const
     int result = android_camera_get_device_info(deviceID, &facing, &orientation);
     if (result != 0) {
         return 0;
+    }
+
+    if (facing == FRONT_FACING_CAMERA_TYPE) {
+        QString deviceCodename = getAndroidDeviceCodename();
+
+        if (deviceCodename == "krillin" || deviceCodename == "vegetahd") {
+            // krillin / vegetahd lies to us - the top of the front facing camera
+            // points to the right of the screen (viewed from the front), which means
+            // the camera image needs rotating by 270deg with the device in its natural
+            // orientation (portrait). It tells us the camera orientation is 90deg
+            // though (see https://launchpad.net/bugs/1567542)
+            // https://git.launchpad.net/oxide/tree/shared/browser/media/oxide_video_capture_device_hybris.cc#n92
+
+            // FIXME: is this the right place to do this?
+            orientation = 270;
+        }
     }
 
     // Android's orientation means differently compared to QT's orientation.